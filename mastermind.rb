e32c6f72da6dfc348bced70061bd5fc114578699
penach0 <josemanuel.penacho@gmail.com>
2022-09-09 15:34:27 +0100

    Add player type and role selection
    -Add functionality to prompt user whether to play vs computer or
    other human.
    -Add functionality to prompt user whether to play as codebreaker
    or codemaker.

--- mastermind.rb
+++ mastermind.rb
@@ -1,6 +1,8 @@
-module UserChecks
+module Input
   POSSIBLE_COLORS = { red: 'R', green: 'G', blue: 'B',
                       yellow: 'Y', white: 'W', purple: 'P' }.freeze
+  KEYS = POSSIBLE_COLORS.keys
+  VALUES = POSSIBLE_COLORS.values
 
   def check_colors(color)
     return color if POSSIBLE_COLORS.key?(color.downcase.to_sym)
@@ -13,21 +15,57 @@ module UserChecks
   def ask_colors
     color = ''
     loop do
+      puts 'Pick a color:'
       color = gets.chomp
       break if check_colors(color)
     end
     color[0].upcase
   end
 
-  def show_possible_colors(line)
-    keys = POSSIBLE_COLORS.keys
-    values = POSSIBLE_COLORS.values
+  def pick_colors(pick_mode)
+    colors = []
+    if pick_mode == 'human'
+      colors << ask_colors while colors.length < 4
+    else
+      colors << VALUES.sample while colors.length < 4
+    end
+    colors
+  end
 
+  def ask_type
+    type = ''
+    loop do
+      puts 'Play vs Human or Computer?'
+      type = gets.chomp
+      break if %w[human computer].include?(type.downcase)
+    end
+    type
+  end
+
+  def ask_role
+    role = ''
+    loop do
+      puts 'Play as Codemaker or Codebreaker?'
+      role = gets.chomp
+      break if %w[codemaker codebreaker].include?(role.downcase)
+    end
+      role
+  end
+end
+
+
+module Output
+  POSSIBLE_COLORS = { red: 'R', green: 'G', blue: 'B',
+                      yellow: 'Y', white: 'W', purple: 'P' }.freeze
+  KEYS = POSSIBLE_COLORS.keys
+  VALUES = POSSIBLE_COLORS.values
+
+  def show_possible_colors(line)
     case line
     when 1
       puts ' Possible colors:'
     when (3..POSSIBLE_COLORS.length + 2)
-      puts " #{values[line - 3]} -> #{keys[line - 3].capitalize} "
+      puts " #{VALUES[line - 3]} -> #{KEYS[line - 3].capitalize} "
     else
       puts ''
     end
@@ -50,23 +88,30 @@ module UserChecks
 end
 
 class Sequence
-  include UserChecks
+  include Input
+  include Output
   attr_reader :colors
 
-  def initialize
-    @colors = []
-    while @colors.length < 4
-      print 'Pick a color: '
-      @colors << ask_colors
-    end
+  def initialize(player_type)
+    @colors = pick_colors(player_type)
+    pp @colors
   end
 end
 
 class Player
+  include Input
+  MAX_PLAYERS = 2
+  attr_reader :type
+
+  def initialize
+    @type = ask_type
+    @role = ask_role
+  end
 end
 
 class Game
-  include UserChecks
+  include Input
+  include Output
   attr_accessor :board, :lines
 
   def initialize
@@ -75,11 +120,11 @@ class Game
 end
 
 class Code < Sequence
-  def initialize
+  def initialize(player_type)
     puts "************************\n"\
          "*****PICK YOUR CODE*****\n"\
          '************************'
-    super
+    super(player_type)
   end
 end
 
@@ -93,5 +138,6 @@ class Guess < Sequence
 end
 
 game = Game.new
-
-code = Code.new
+player1 = Player.new
+puts player1.type.inspect
+code = Code.new(player1.type)
